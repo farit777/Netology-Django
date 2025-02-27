5fa9ba54db7814a3c053da84cd84ae90caed5dbe
farit777 <farit777@mail.ru>
2025-02-27 20:12:56 +0300

    Completed HW

--- 1.1_first_project/app/views.py
+++ 1.1_first_project/app/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render, reverse
+from datetime import datetime
+import os
 
 
 def home_view(request):
@@ -11,7 +13,7 @@ def home_view(request):
         'Показать текущее время': '',
         'Показать содержимое рабочей директории': ''
     }
-    
+
     # context и параметры render менять не нужно
     # подбробнее о них мы поговорим на следующих лекциях
     context = {
@@ -21,15 +23,20 @@ def home_view(request):
 
 
 def time_view(request):
-    # обратите внимание – здесь HTML шаблона нет, 
-    # возвращается просто текст
-    current_time = None
-    msg = f'Текущее время: {current_time}'
-    return HttpResponse(msg)
+    # Получаем текущее время
+    now = datetime.now()
+    # Форматируем текущее время как строку
+    time_str = now.strftime('%Y-%m-%d %H:%M:%S')
+    # Возвращаем ответ с текущим временем
+    return HttpResponse(f'Текущее время: {time_str}')
 
 
 def workdir_view(request):
-    # по аналогии с `time_view`, напишите код,
-    # который возвращает список файлов в рабочей 
-    # директории
-    raise NotImplemented
+    # Получаем текущую рабочую директорию
+    current_dir = os.getcwd()
+    # Получаем список файлов и директорий в текущей директории
+    items = os.listdir(current_dir)
+    # Форматируем список в строку для вывода
+    items_str = "<br>".join(items)  # Преобразуем в HTML-формат (построчно)
+    # Возвращаем ответ с содержимым рабочей директории
+    return HttpResponse(f'Текущая рабочая директория: {current_dir}<br>Содержимое:<br>{items_str}')
